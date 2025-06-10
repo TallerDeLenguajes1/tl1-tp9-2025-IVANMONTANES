17bca718d6e8a0b9e906b5bd36e48244ff1908a2
ivan <ivanmontanesfilipit@gmail.com>
2025-06-10 11:53:44 -0300

    pedir path y que determine si existe o no

---
+++ lectorDirectorio/Program.cs
@@ -0,0 +1,33 @@
+﻿// bucle hasta que se ingrese un path valido //
+bool existePath = false;
+while (!existePath) {
+    // pedimos que se ingrese el path no vacio //
+    bool pathCargado = false;
+    string pathIngresado = string.Empty;
+    do
+    {
+        Console.WriteLine("=========================");
+        Console.WriteLine("Ingrese el path:");
+        pathIngresado = Console.ReadLine();
+        if (string.IsNullOrEmpty(pathIngresado))
+        {
+            Console.WriteLine("ingrese una cadena no vacia");
+        }
+        else
+        {
+            pathCargado = true;
+        }
+        Console.WriteLine("=========================");
+    } while (pathCargado == false);
+
+    // determinamos si el directorio existe o si no existe //
+    if (Directory.Exists(pathIngresado))
+    {
+        Console.WriteLine("se encontro el directorio");
+        existePath = true;
+    }
+    else
+    {
+        Console.WriteLine("no se encontro el directorio, porfavor ingrese un path existente");
+    }
+}
