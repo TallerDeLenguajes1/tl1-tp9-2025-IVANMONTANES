0b6f43870a9c14cbc9e80b78458647ac5be3cdf3
ivan <ivanmontanesfilipit@gmail.com>
2025-06-11 00:58:18 -0300

    punto 2 terminado

---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using clases;
+FileStream archivo = File.Open("archivo.mp3", FileMode.Open);
+archivo.Seek(-128, SeekOrigin.End);
+// arreglos de bytes para ir guardando la informacion //
+byte[] header = new byte[3];
+byte[] titulo = new byte[30];
+byte[] Artista = new byte[30];
+byte[] Album = new byte[30];
+byte[] anio = new byte[4];
+byte[] comentario = new byte[30];
+byte[] genero = new byte[1];
+// vamos leyendo el archivo y almacenando en cada arreglo //
+archivo.Read(header, 0, 3);
+archivo.Read(titulo, 0, 30);
+archivo.Read(Artista, 0, 30);
+archivo.Read(Album, 0, 30);
+archivo.Read(anio, 0, 4);
+archivo.Read(comentario, 0, 30);
+archivo.Read(genero, 0, 1);
+// convertimos los arreglos de bytes a cadenas de caracteres //
+string headerTitulo = Encoding.ASCII.GetString(titulo);
+string headerArtista = Encoding.ASCII.GetString(Artista);
+string headerAlbum = Encoding.ASCII.GetString(Album);
+string headerAnio = Encoding.ASCII.GetString(anio);
+// creamos el objeto y lo mostramos //
+Id3v1Tag tag = new Id3v1Tag(headerTitulo, headerArtista, headerAlbum, headerAnio);
+tag.mostrarMetadatos();
+
---
+++ LectorTagMP3/clases.cs
@@ -0,0 +1,27 @@
+﻿namespace clases
+{
+    public class Id3v1Tag
+    {
+        public Id3v1Tag(string titulo, string artista, string album, string anioPublicacion)
+        {
+            Titulo = titulo;
+            Artista = artista;
+            Album = album;
+            AnioPublicacion = anioPublicacion;
+        }
+
+        public string Titulo { get; set; }
+        public string Artista { get; set; }
+        public string Album { get; set; }
+        public string AnioPublicacion { get; set; }
+
+        // metodo para mostrar el tag //
+        public void mostrarMetadatos()
+        {
+            Console.WriteLine($"TITULO: {Titulo}");
+            Console.WriteLine($"ARTISTA: {Artista}");
+            Console.WriteLine($"ALBUM: {Album}");
+            Console.WriteLine($"AÑO DE LA CANCION: {AnioPublicacion}");
+        }
+    }
+}
